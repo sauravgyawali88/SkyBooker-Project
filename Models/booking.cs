f7e689aad4860376fd142e432891e09957a30b8f
sauravgyawali88 <gyawalisaurav892@gmail.com>
2025-03-27 20:17:07 -0400

    Added Booking Model Class
    This class represents a flight booking in the SkyBooker app. It includes important details like `BookingID`, `UserID`, `FlightID`, `BookingDate`, `PaymentStatus`, and `TotalAmount`. It’s used to manage and track bookings, linking users to flights and keeping track of payment and booking information.

---
+++ Models/booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyBooker_Project.Models
+{
+    public class Booking
+    {
+        public string BookingID { get; set; }  // Unique ID for the booking
+        public string UserID { get; set; }  // ID of the user who made the booking
+        public string FlightID { get; set; }  // ID of the flight that was booked
+        public DateTime BookingDate { get; set; }  // Date and time when the booking was made
+        public bool PaymentStatus { get; set; }  // Whether the payment was successful (true/false)
+        public decimal TotalAmount { get; set; }  // Total cost of the booking
+
+        // Navigation properties to link to the User and Flight models
+        public User User { get; set; }  // The user who made the booking
+        public Flight Flight { get; set; }  // The flight that was booked
+    }
+}
