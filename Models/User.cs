89ed371bd2b62a53b9867eb0bcdc30ba279b9e99
sauravgyawali88 <gyawalisaurav892@gmail.com>
2025-03-27 20:05:27 -0400

    Added User Model Class. This class represents a user in the SkyBooker app. It includes important details like `UserID`, `UserName`, `Email`, `Password`, `FrequentFlyerPoints`, and `PreferredAirline`. This model will be used to manage user accounts and provide personalized flight booking experiences.

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyBooker_Project.Models
+{
+    public class User
+    {
+        public string UserID { get; set; }  // Unique ID for the user
+        public string UserName { get; set; }  // Full name of the user
+        public string Email { get; set; }  // User's email address
+        public string Password { get; set; }  // Password for the user
+        public int FrequentFlyerPoints { get; set; }  // Points the user has accumulated
+        public string PreferredAirline { get; set; }  // The user's preferred airline
+    }
+}
